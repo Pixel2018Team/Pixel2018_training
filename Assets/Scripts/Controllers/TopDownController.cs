51852e8c385ea8c814b20c12223e55f75ab4ad94
Samuel <carriere911@gmail.com>
2018-03-22 19:47:36 -0400

    add the base rotation for the twin stick

--- Assets/Scripts/Controllers/TopDownController.cs
+++ Assets/Scripts/Controllers/TopDownController.cs
@@ -15,8 +15,16 @@ public class TopDownController : MonoBehaviour
 
     void Update()
     {
+        Debug.DrawLine(transform.position, transform.position + transform.rotation * Vector3.forward * 3, Color.red);
+
         _moveInput = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical"));
         _moveVelocity = _moveInput * speed;
+
+        Vector3 newDirection = Vector3.right * Input.GetAxisRaw("RHorizontal") + Vector3.forward * Input.GetAxisRaw("RVertical");
+        if(newDirection.sqrMagnitude > 0.0f)
+        {
+            transform.rotation = Quaternion.LookRotation(newDirection, Vector3.up);
+        }
     }
 
     private void FixedUpdate()
