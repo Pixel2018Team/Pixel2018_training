c7764f83b2a1252e71598baedae13c1deaab84b5
Leandre <leandremonette@gmail.com>
2018-04-01 01:28:34 -0700

    Wwise integration ! Un son devrait jouer au moment ou on commence la partie dans la scene TopDownTest

---
+++ Assets/Scripts/Audio/AudioTest.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioTest : MonoBehaviour {
+
+	void Start(){
+		AkSoundEngine.PostEvent ("test", gameObject);
+	}
+}
